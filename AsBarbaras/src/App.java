900faeaac3ff05dfd6b4b4e95481374ad73e65fd
Gabriel Quintana <gabrielquintana2003@icloud.com>
2022-11-18 11:10:11 -0300

    arvore tres

--- AsBarbaras/src/App.java
+++ AsBarbaras/src/App.java
@@ -4,28 +4,26 @@ public class App {
         GenericTree gt = new GenericTree();
         gt.readFile("exemplo.txt");
         gt.distribuicaoTerras();
-        System.out.println("\n\nBarbaro que possui mais terras na árvore genealógica 1:");
+        System.out.println("\nBarbaro que possui mais terras na árvore genealógica 1:");
         System.out.println(gt.maisTerras());
 
         GenericTree gt1 = new GenericTree();
         gt1.readFile("ArvoreUm.txt");
         gt1.distribuicaoTerras();
-        gt1.LevelOrderTraversal(gt1.getRoot());
-        System.out.println("\n\nBarbaro que possui mais terras na árvore genealógica 2:");
+        System.out.println("\nBarbaro que possui mais terras na árvore genealógica 2:");
         System.out.println(gt1.maisTerras());
 
         GenericTree gt2 = new GenericTree();
         gt2.readFile("ArvoreDois.txt");
         gt2.distribuicaoTerras();
-        gt2.LevelOrderTraversal(gt2.getRoot());
-        System.out.println("\n\nBarbaro que possui mais terras na árvore genealógica 3:");
+        System.out.println("\nBarbaro que possui mais terras na árvore genealógica 3:");
         System.out.println(gt2.maisTerras());
 
-        // GenericTree gt3 = new GenericTree();
-        // gt3.readFile("exemplo.txt");
-        // gt3.distribuicaoTerras();
-        // System.out.println("\n\nBarbaro que possui mais terras na árvore genealógica 4:");
-        // System.out.println(gt3.maisTerras());
+        GenericTree gt3 = new GenericTree();
+        gt3.readFile("ArvoreTres.txt");
+        gt3.distribuicaoTerras();
+        System.out.println("\nBarbaro que possui mais terras na árvore genealógica 4:");
+        System.out.println(gt3.maisTerras());
 
         // GenericTree gt4 = new GenericTree();
         // gt4.readFile("exemplo.txt");
