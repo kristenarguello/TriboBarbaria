b19c15126bbe4a53413e1eb19598a8dc1247e455
Kristen <kristenarguello20@gmail.com>
2022-11-19 14:36:44 -0300

    Apresentacao

--- AsBarbaras/src/App.java
+++ AsBarbaras/src/App.java
@@ -5,31 +5,31 @@ public class App {
         GenericTree gt = new GenericTree();
         gt.readFile("exemplo.txt");
         gt.distribuicaoTerras();
-        System.out.println("\nÁRVORE GENEALÓGICA 1: " + gt.maisTerras());
+        System.out.println("\nÁRVORE GENEALÓGICA DE EXEMPLO: " + gt.maisTerras());
         System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
 
         GenericTree gt1 = new GenericTree();
         gt1.readFile("ArvoreUm.txt");
         gt1.distribuicaoTerras();
-        System.out.println("\nÁRVORE GENEALÓGICA 2: " + gt1.maisTerras());
+        System.out.println("\nÁRVORE GENEALÓGICA 1: " + gt1.maisTerras());
         System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
 
         GenericTree gt2 = new GenericTree();
         gt2.readFile("ArvoreDois.txt");
         gt2.distribuicaoTerras();
-        System.out.println("\nÁRVORE GENEALÓGICA 3: " + gt2.maisTerras());
+        System.out.println("\nÁRVORE GENEALÓGICA 2: " + gt2.maisTerras());
         System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
 
         GenericTree gt3 = new GenericTree();
         gt3.readFile("ArvoreTres.txt");
         gt3.distribuicaoTerras();
-        System.out.println("\nÁRVORE GENEALÓGICA 4: " + gt3.maisTerras());
+        System.out.println("\nÁRVORE GENEALÓGICA 3: " + gt3.maisTerras());
         System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
 
         GenericTree gt4 = new GenericTree();
         gt4.readFile("ArvoreQuatro.txt");
         gt4.distribuicaoTerras();
-        System.out.println("\nÁRVORE GENEALÓGICA 5: " + gt4.maisTerras());
+        System.out.println("\nÁRVORE GENEALÓGICA 4: " + gt4.maisTerras());
         
     }
 
